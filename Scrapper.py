42fc9139d0ace068bd58a4793d0ecf187b2ba23e
Mohammad Zahin Faiyaz Islam <zahin.fyz@gmail.com>
2022-12-03 23:24:15 +0600

    commit 3

--- Scrapper.py
+++ Scrapper.py
@@ -12,12 +12,10 @@ soup = BeautifulSoup(request_web_page.text , 'html.parser')
 #print(soup)
 
 
-
 #First Task get all the images
-
 images = soup.find_all('img')
-#print(images)
 
+#making or directring to existing folder
 try:
     #Either you make a directory
     os.mkdir(os.path.join(os.getcwd(), 'Images'))
@@ -26,22 +24,23 @@ except:
 #Or write to an existing one
 os.chdir(os.path.join(os.getcwd(), 'Images'))
 
+#looping to get image name and the image
 for image in images :
+    #ekhane amra name nicchi of the person from src
     name = image['src'][9:-5]
+    #jehetu website ei images gula and prottek ta image er different link
+    #tai common part of the link likhe , specific emage tag add kortesi "image['src'][1:23]"
+    # 1 ro 23 cause name can be big smaller values can be used
     link = 'https://zahinfyz.github.io/Nsu-Student-Database.github.io'+image['src'][1:23]
-    #print(name)
-    #print(link)
+
+    #writing the images in the folder
     with open(name + '.jpeg', 'wb') as f:
         im = requests.get(link)
         f.write(im.content )
-        print('Writing: ', name)
-
+        print('Downloaded Image :', name)
 
 
 os.chdir(r"F:\This Semester\Python\Projects\Face Recognition")
-web_page = BeautifulSoup(request_web_page.content , 'html.parser')
-wl = [web_page]
-#print(wl)
 
 lists = soup.find_all('td')
 
@@ -72,8 +71,4 @@ keep_col = ['Name','Email','ID','Phone']
 new_f = f[keep_col]
 new_f.to_csv("Student Database.csv", index=False)
 
-
-#for list in lists :
-    #title = list.find('tr')
-    #title1 = list.find('td')
-    #print(title)'''
+import Face_Recognition
